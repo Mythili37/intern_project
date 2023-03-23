a6e2cab5f104e5f4e3812bb479550da03f12f001
w3schools-test <test@w3schools.com>
2023-03-23 14:50:44 +0530

    two programs added

---
+++ Logesh/program1.py
@@ -0,0 +1,8 @@
+n = int(input("Enter the size (minimun 8):"))
+for i in range(n):
+    for j in range(n):
+        if i == j or j == 0 or j == n-1:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print("\n")
