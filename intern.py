15ddc4ac01b4397dfeaf5b03e21dad5d2fca1353
ANORA\logesh <logesh@anoralabs.com>
2023-03-24 13:56:49 +0530

    Subtraction function added

--- intern.py
+++ intern.py
@@ -1,25 +1,36 @@
 def default_function():
     print("Hello! Welcome to Menu driven Program")
 
+
 def add(a,b):
     print(a+b)
+
+
+def sub(a, b):
+    print(f"Subtraction of {a} and {b} is :", a-b)
+
+
 def mul(a,b):
     print(a*b)
 
+
 def div(a, b):
     print(a / b)
+
+
 if __name__ == "__main__":
 
     print("0. Exit")
 
     print("1. Default Choice")
     print("2. Addition")
+    print("3. Subtraction")
     print("4.Muiltiplication")
     print("5. Division")
 
     while True:
 
-        choice = int(input("Enter a choice"))
+        choice = int(input("Enter a choice :"))
 
         if not choice:
             break
@@ -27,16 +38,20 @@ if __name__ == "__main__":
         if choice == 1:
             default_function()
         elif choice == 2:
-            inp1 = int(input("enter the number"))
+            inp1 = int(input("enter the number "))
             inp2 = int(input("enter the number"))
-            add(inp1,inp2)
+            add(inp1, inp2)
+        elif choice == 3:
+            inp1 = int(input("enter the number :"))
+            inp2 = int(input("enter the number :"))
+            sub(inp1, inp2)
         elif choice==4:
             inp1 = int(input("enter the number"))
             inp2 = int(input("enter the number"))
-            mul(inp1,inp2)
+            mul(inp1, inp2)
         elif choice == 5:
             inp1 = int(input("enter the number"))
             inp2 = int(input("enter the number"))
-            div(inp1,inp2)
+            div(inp1, inp2)
 
     print("Thanks! Come Again")
